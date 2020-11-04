df742e41e1af8b143241af691993d232beb7a353
charlieholy <daad777@126.com>
2020-11-04 20:55:34 +0800

    Update downloads.sh
    add ndk url

--- downloads.sh
+++ downloads.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 wget https://download.sublimetext.com/Sublime%20Text%20Build%203211%20x64%20Setup.exe
+wget https://dl.google.com/android/repository/android-ndk-r14b-darwin-x86_64.zip #for ijkplayer
