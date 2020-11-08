84cca0ae92a900468c66abaf00d9799fec8b02ed
charlieholy <daad777@126.com>
2020-11-08 23:46:44 +0800

    Update downloads.sh

--- downloads.sh
+++ downloads.sh
@@ -3,3 +3,4 @@ wget https://download.sublimetext.com/Sublime%20Text%20Build%203211%20x64%20Setu
 wget https://dl.google.com/android/repository/android-ndk-r14b-darwin-x86_64.zip #for ijkplayer
 wget https://1.na.dl.wireshark.org/win64/Wireshark-win64-3.4.0.exe. #for wireshark
 wget https://services.gradle.org/distributions/gradle-2.14.1-all.zip
+wget http://dl.google.com/android/ndk/android-ndk-r10d-darwin-x86_64.bin 
