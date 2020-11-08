a8a1f77c9f156e72d196d01dd439573063941d69
charlieholy <daad777@126.com>
2020-11-08 14:08:55 +0800

    Create ijkplayer.sh

---
+++ ijkplayer.sh
@@ -0,0 +1,28 @@
+cd android/contrib
+./compile-ffmpeg.sh arm64
+cd -
+cd android
+./compile-ijk.sh arm64
+cd -
+
+mod ijkplayer-example build.gradle
+    productFlavors {
+        //all32 { minSdkVersion 9 }
+        all64 { minSdkVersion 21 }
+        // armv5 {}
+        // armv7a {}
+        arm64 { minSdkVersion 21 }
+        // x86 {}
+    }
+    dependencies {
+    compile fileTree(include: ['*.jar'], dir: 'libs')
+    compile 'com.android.support:appcompat-v7:23.0.1'
+    compile 'com.android.support:preference-v7:23.0.1'
+    compile 'com.android.support:support-annotations:23.0.1'
+
+    compile 'com.squareup:otto:1.3.8'
+
+    compile project(':ijkplayer-java')
+    compile project(':ijkplayer-exo')
+    all64Compile project(':ijkplayer-arm64')
+    }
