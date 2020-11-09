2760bbaa2688c7142bbedd6ad6b3dadafb8bcc47
charlieholy <daad777@126.com>
2020-11-09 23:49:02 +0800

    Update gradle.sh

--- gradle.sh
+++ gradle.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 gradle tasks --list
 gradle :entry:clean --info
+gradle enrty:compileDebugNativeWithCmake --info
+
+add_definition(-DDEBUG -DOHOSME)
 
 #gradle 插件安装路径 caches/modules-2
 jad -o -r -ff -dtest -sjava -sjava structs2-core/**/*.class
