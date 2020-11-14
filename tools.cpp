a021edf511f2a44becc714e2dc20af8ef24d6dd7
charlieholy <daad777@126.com>
2020-11-14 11:07:55 +0800

    Update tools.cpp

--- tools.cpp
+++ tools.cpp
@@ -10,3 +10,18 @@
 #define LOGD(fmt,...) LOGO("\033[0;33;40m", "DEBUG", fmt, ##__VA_ARGS__)
 #define LOGI(fmt,...) LOGO("\033[0;32;40m", "INFO ", fmt, ##__VA_ARGS__)
 #define LOGE(fmt,...) LOGO("\033[0;31;40m", "ERROR", fmt, ##__VA_ARGS__)
+
+
+#define ALOG(level, TAG, ...)  do{\
+  __android_log_with_line(level,TAG,__FILE__,__LINE__,__VA_ARGS__);\
+  }while(0)
+
+void __android_log_with_line(int level, const char *tag, const char* file, int line, const char *fmt, ...)
+{
+    char buf[2048] = {0};
+    va_list args;
+    va_start(args,fmt);
+    vsnprintf(buf,2048,fmt,args);
+    __android_log_print(level,tag,"%s:%d %s",file,line,buf);
+}
+
